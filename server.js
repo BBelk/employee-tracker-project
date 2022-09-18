57da4a25061f65f2e64309a1283ae5bd3c8d15b5
Belkbh <belkbh@gmail.com>
2022-09-18 01:53:02 -0400

    Add department and role finished

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ function ViewAllDepartments(){
 }
 
 function ViewAllRoles(){
-    db.query('SELECT * FROM role', (error, response) => {
+    db.query('SELECT role.title, role.id, department.name AS department, role.salary FROM role INNER JOIN department ON role.department_id = department.id;', (error, response) => {
         if (error) throw error;
         console.log('\n');
         console.log("ROLES");
@@ -99,7 +99,7 @@ function ViewAllRoles(){
 }
 
 function ViewAllEmployess(){
-    db.query('SELECT * FROM employee', (error, response) => {
+    db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title AS job_title, department.name AS department, role.salary AS salary, employee.manager_id AS manager_id FROM employee, role, department WHERE department.id = role.department_id AND role.id = employee.role_id;', (error, response) => {
         if (error) throw error;
         console.log('\n');
         console.log("EMPLOYEES");
@@ -123,19 +123,73 @@ function AddADepartment(){
             console.log('\n');
             console.log("Department added succesfully");
             DoLine();
-            console.table(response);
-            DoLine();
+            // console.table(response);
+            // DoLine();
             MainMenu();
             });
     });
 }
 
 function AddARole(){
-    // inquirer.prompt
+    inquirer.prompt([
+        {
+            name: 'roleTitle',
+            message : 'Enter New Role Title'
+        },
+        {
+            name: 'roleSalary',
+            message: 'Enter New Role Salary'
+        },
+        {
+            name: 'roleDepartment',
+            message: 'Enter New Department'
+        }
+    ])
+    .then(response => {
+        db.query(`INSERT INTO role (title, salary, department_id) VALUES("${response.roleTitle}", "${response.roleSalary}", "${response.roleDepartment}");`, (error, response) => {
+            if (error) throw error;
+            console.log('\n');
+            console.log("Role added succesfully");
+            DoLine();
+            // console.table(response);
+            // DoLine();
+            MainMenu();
+            });
+    });
 }
 
 function AddAnEmployee(){
-    // inquirer.prompt
+    inquirer.prompt([
+        {
+            name: 'employeeFirstName',
+            message : "Enter New Employee's First Name"
+        },
+        {
+            name: 'employeeLastName',
+            message : "Enter New Employee's Last Name"
+        },
+        {
+            name: 'employeeRole',
+            message: "Enter New Employee's Role ID"
+        },
+        {
+            name: 'employeeManager',
+            message: "Enter New Employee's Manager ID, if none enter 0",
+            default: "0"
+        }
+    ])
+    .then(response => {
+        if(response.employeeManager == "0"){response.employeeManager = "null";}
+        db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES("${response.employeeFirstName}", "${response.employeeLastName}", "${response.employeeRole}", "${response.employeeManager}");`, (error, response) => {
+            if (error) throw error;
+            console.log('\n');
+            console.log("Employee added succesfully");
+            DoLine();
+            // console.table(response);
+            // DoLine();
+            MainMenu();
+            });
+    });
 }
 
 function UpdateAnEmployeeRole(){
